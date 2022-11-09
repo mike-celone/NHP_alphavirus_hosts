98dff8ec85289ac30acccb5a67b02ba782818490
Mike Celone <mikecelone1@gmail.com>
2022-11-09 00:22:21 -0500

    New commit

--- GBM_model_8NOV22.R
+++ GBM_model_8NOV22.R
@@ -1,15 +1,19 @@
-#---Set parameters---#
-nruns <- 5
+#Code to run bootstrapped GBM models
+#Optimal parameters determined from a grid search
+
+#-Set parameters-#
+nruns <- 10
 eta <- 0.01          
 max_depth <- 2               
 n.minobsinnode <- 2       
 k_split <- 0.8                        
-nrounds <- 2000 
-#####################
+nrounds <- 100000
+cv.folds <- 5
+#-----------------#
 
-#----Run the bootstrap GBM-----#
 #Create train/test partitions for 10 model runs
-DaFr <- foreach(i = 1:nruns) %do% {
+df <- foreach(i = 1:nruns) %do% {
+  #Set a seed for each model
   set.seed(i)
   
   dp <- createDataPartition(prim_data_final$Label, p = k_split)[[1]]
@@ -19,14 +23,14 @@ DaFr <- foreach(i = 1:nruns) %do% {
   list(train, test)
 }
 
-#Run the model 10 times 
+#Run the 10 models 
 gbm.list <- lapply(1:nruns, function(m) {
-  gbm(data= DaFr[[m]][[1]],
+  gbm(data= df[[m]][[1]],
       formula = Label ~ .,
       distribution = "bernoulli",
       n.trees = nrounds,
       shrinkage = eta,
-      cv.folds = 5,
+      cv.folds = cv.folds,
       interaction.depth = max_depth,
       n.minobsinnode = n.minobsinnode,
       bag.fraction = 0.5,
@@ -52,31 +56,37 @@ predictions <- do.call(rbind, lapply(1:nruns, function(j) {
              bootstrap_run = j,
              original_value = prim_data_final[, label])}))
 
-output2 <- lapply(1:length(gbm.list), function(x) cbind(predict(gbm.list[[x]],
-                                                                newdata = DaFr[[x]][[1]],
+output1 <- lapply(1:length(gbm.list), function(x) cbind(predict(gbm.list[[x]],
+                                                                newdata = df[[x]][[1]],
                                                                 n.trees = best.iter[x],
                                                                 type = "response"),
-                                                        as.numeric(unlist(DaFr[[x]][[1]][, label]))))
+                                                        as.numeric(unlist(df[[x]][[1]][, label]))))
 
-eval_train <- sapply(output2, function(x) colAUC(x[,1],x[,2]))
-rmse_train <- sapply(1:length(gbm.list), function(x) Metrics::rmse(actual = DaFr[[x]][[1]][, label],
+#AUC and RMSE for training data
+eval_train <- sapply(output1, function(x) colAUC(x[,1],x[,2]))
+
+rmse_train <- sapply(1:length(gbm.list), function(x) Metrics::rmse(actual = df[[x]][[1]][, label],
                                                                    predicted = predict(gbm.list[[x]],
                                                                                        newdata = DaFr[[x]][[1]],
                                                                                        n.trees = best.iter[x],
                                                                                        type = "response")))
+
 output2 <- lapply(1:length(gbm.list), function(x) cbind(predict(gbm.list[[x]],
                                                                 newdata = DaFr[[x]][[2]],
                                                                 n.trees = best.iter[x],
                                                                 type = "response"),
                                                         as.numeric(unlist(DaFr[[x]][[2]][, label]))))
+
+#AUC and RMSE for testing data
 eval_test <- sapply(output2, function(x) colAUC(x[,1],x[,2]))
+
 rmse_test <- sapply(1:length(gbm.list), function(x) Metrics::rmse(actual = DaFr[[x]][[2]][, label],
                                                                   predicted = predict(gbm.list[[x]],
                                                                                       newdata = DaFr[[x]][[2]],
                                                                                       n.trees = best.iter[x],
                                                                                       type = "response")))
 
-
+#Relative importance scores for the variables
 df_importance <- do.call(rbind, 
                          lapply(gbm.list, 
                                 function(j) t(as.data.frame(summary(j)$rel.inf[match(vars, summary(j)$var)], 
@@ -92,6 +102,7 @@ pd_out <- foreach(i = 1:nruns, .combine = "rbind") %do% {
                                                    variable.name = m,
                                                    effect = "marginal.effect",
                                                    bootstrap_run = i))
+  
   pd_out <- do.call(rbind, lapply(pd_out, function(m) {
     colnames(m)[1:2] <- c("x", "yhat")
     m}))
@@ -108,4 +119,5 @@ out1 <- cbind.data.frame(eta = eta,
                          rmse_test = rmse_test,
                          bootstrap_run = 1:nruns,
                          df_importance)
-lst<-list(out1, pd_out, predictions)
+
+final.list <- list(out1, pd_out, predictions)
